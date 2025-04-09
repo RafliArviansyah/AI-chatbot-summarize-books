b4c076f1b5a4c60e0c70816869b8f8a43bbd0644
Rafli Altair <fianzah43@gmail.com>
2025-04-09 15:50:50 +0700

    Update chatbotpdfrag_final.py

--- chatbotpdfrag_final.py
+++ chatbotpdfrag_final.py
@@ -12,7 +12,7 @@ from gtts import gTTS
 import os
 
 # API Key
-OPENROUTER_API_KEY = "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"
+OPENROUTER_API_KEY = "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"
 
 # Konfigurasi Halaman
 st.set_page_config(page_title="Chatbot Self Improvement", layout="wide")
